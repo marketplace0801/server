218ee218579bbca73624a4c9a777554e7996e550
hazrath <hazrath@localhost.localdomain>
2022-09-02 15:14:23 +0530

    added routes.txt

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,6 +2,6 @@ import express from "express";
 import { createProduct, getAllProduct } from "../controller/productController.js";
 const router = express.Router();
 import{ isAuthenticated }from '../middleware/auth.js'
-router.route('/getAllProducts').get(isAuthenticated, getAllProduct)
-router.route('/createProduct').post(isAuthenticated, createProduct)
+router.route('/getallproducts').get(isAuthenticated, getAllProduct)
+router.route('/createproduct').post(isAuthenticated, createProduct)
 export default router
